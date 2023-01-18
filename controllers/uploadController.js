b1285f20e17f3cc2499ea3263adb19f56d684946
shresthasafal1 <shresthasafal000@gmail.com>
2023-01-18 18:57:55 +0545

    feat: file upload and save in a folder

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -1,6 +1,16 @@
+const path = require("path");
+
 exports.fileUpload = (req, res) => {
   const files = req.files;
   console.log(files);
+
+  //save the files
+  Object.keys(files).forEach((key) => {
+    const filepath = path.join(__dirname, "..", "files", files[key].name);
+    files[key].mv(filepath, (err) => {
+      if (err) return res.send("no file uploaded");
+    });
+  });
 
   return res.json({ status: "success", message: "success" });
 };
--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const fileUpload = require("express-fileupload");
 const path = require("path");
-const uploadRoute = require("./routes/uploadRoute")
+const uploadRoute = require("./routes/uploadRoute");
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "upload.html"));
 });
 
-app.use("/upload", fileUpload({ createParentPath: true }), uploadRoute);
+app.use(
+  "/upload",
+  fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 5 * 1024 * 1024 },
+  }),
+  uploadRoute
+);
 
 app.listen(3000);
